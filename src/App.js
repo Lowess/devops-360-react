dd8004e35bd7ab900447bc66e742fe65c452c401
Florian Dambrine <florian@gumgum.com>
2018-03-21 23:50:40 -0700

    Supress warnings

--- src/App.js
+++ src/App.js
@@ -4,42 +4,47 @@ import './App.css';
 
 import GoGear from 'react-icons/lib/go/gear'
 
-// From https://coolors.co/app/52489c-ef476f-ffd166-86bbd8-06d6a0
-const palette_set_1 = [
-  "rgba(207, 255, 179, 1)",
-  "rgba(173, 226, 93, 1)",
-  "rgba(253, 196, 41, 1)",
-  "rgba(59, 112, 128, 1)",
-  "rgba(58, 87, 67, 1)"
-]
+const palettes = {
+  // From https://coolors.co/app/52489c-ef476f-ffd166-86bbd8-06d6a0
+  "set_1": [
+    "rgba(207, 255, 179, 1)",
+    "rgba(173, 226, 93, 1)",
+    "rgba(253, 196, 41, 1)",
+    "rgba(59, 112, 128, 1)",
+    "rgba(58, 87, 67, 1)"
+  ],
 
-// From https://coolors.co/app/006d75-00c1b5-c9e8dd-c6003d-4f213a
-const palette_set_2 = [
-  "rgba(50, 199, 135, 1)",
-  "rgba(171, 225, 136, 1)",
-  "rgba(63, 167, 214, 1)",
-  "rgba(250, 192, 94, 1)",
-  "rgba(247, 239, 153, 1)"
-]
+  // From https://coolors.co/app/006d75-00c1b5-c9e8dd-c6003d-4f213a
+  "set_2": [
+    "rgba(50, 199, 135, 1)",
+    "rgba(171, 225, 136, 1)",
+    "rgba(63, 167, 214, 1)",
+    "rgba(250, 192, 94, 1)",
+    "rgba(247, 239, 153, 1)"
+  ]
+}
 
-const devops_subjects = [
-  "Automation",
-  "Infrastructure as Code",
-  "Continuous integration",
-  "Continuous deployment",
-  "Working with Developers",
-  "Monitoring",
-  "Alerting"
-]
+const subjects = {
+  "devops": [
+    "Automation",
+    "Infrastructure as Code",
+    "Continuous integration",
+    "Continuous deployment",
+    "Working with Developers",
+    "Monitoring",
+    "Alerting"
+ ]
+}
+
 
 class App extends Component {
   constructor(props){
     super(props);
 
     // Set the list of subjects to display
-    this.subjects = devops_subjects
+    this.subjects = subjects["devops"]
     // Set the palette style
-    this.palette = palette_set_1
+    this.palette = palettes["set_1"]
 
 
     this.state = {
