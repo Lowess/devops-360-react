fa7eb988e3483139e10be1713d19700afe4c1f96
Florian Dambrine <florian@gumgum.com>
2018-04-03 14:50:08 -0700

    Set color to yellow

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
     this.subjects = subjects["devops"]
 
     // Set the palette style
-    this.palette = palettes["blue"]
+    this.palette = palettes["yellow"]
 
     this.state = {
       subject: this.randomSubject(this.subjects),
